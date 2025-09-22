df94ef0ded5a013ddb367af87273801141003708
William Matton <williammatton@gmail.com>
2025-09-22 13:23:21 +0200

    Refactor main.js: Improve function for deleting todo items, rename function for completing todo items, and ensure consistent use of double quotation marks.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-const textInput = document.querySelector('#textInput');
-const submitButton = document.querySelector('#submitButton');
-const alertText = document.querySelector('#alertText');
-const itemList = document.querySelector('#itemList');
-const completedItemsLabel = document.querySelector('#completedItemsLabel');
+const textInput = document.querySelector("#textInput");
+const submitButton = document.querySelector("#submitButton");
+const alertText = document.querySelector("#alertText");
+const itemList = document.querySelector("#itemList");
+const completedItemsLabel = document.querySelector("#completedItemsLabel");
 let listItems = [];
 
 let completedItemsCount = 0;
 
-submitButton.addEventListener('click', SubmitTodoItem);
+submitButton.addEventListener("click", SubmitTodoItem);
 
 function SubmitTodoItem() {
     alertText.textContent = "";
@@ -17,16 +17,17 @@ function SubmitTodoItem() {
         return;
     }
 
-    const entry = document.createElement('li');
-    const deleteButton = document.createElement('span');
-    const taskTextElement = document.createElement('span');
+    const entry = document.createElement("li");
+    const deleteButton = document.createElement("span");
+    const taskTextElement = document.createElement("span");
 
     const item = {
         textElement: taskTextElement,
+        name: task,
         completed: false,
         updateStatus: function() {
             this.completed = !this.completed;
-            this.textElement.classList.toggle('completed');
+            this.textElement.classList.toggle("completed");
             
             this.completed ? completedItemsCount++ : completedItemsCount--;
             UpdateTaskCounter();
@@ -34,43 +35,39 @@ function SubmitTodoItem() {
     };
     listItems.push(item);
     
-    deleteButton.innerHTML = '&#x1F5D1';
-    deleteButton.addEventListener('click', function() {
-        DeleteTodoItem(item, entry, deleteButton);
-    });
-    entry.appendChild(deleteButton);
-    
+    // Todo text
     taskTextElement.textContent = task;
-    taskTextElement.addEventListener('click', function() {
-        CompleteTodoItem(item);
+    taskTextElement.addEventListener("click", function() {
+        UpdateTodoItem(item);
     });
     entry.appendChild(taskTextElement);
 
+    // Delete button
+    deleteButton.innerHTML = "&#x1F5D1";
+    deleteButton.addEventListener("click", function() {
+        DeleteTodoItem(item, entry);
+    });
+    entry.appendChild(deleteButton);
+
     itemList.appendChild(entry);
 
-    textInput.value = '';
+    textInput.value = "";
 }
 
-function CompleteTodoItem(task) {
-    task.updateStatus();
+function UpdateTodoItem(item) {
+    item.updateStatus();
 }
 
-function DeleteTodoItem(item, entry, deleteButton) {
-    entry.remove();
-    deleteButton.remove();
-    
+function DeleteTodoItem(item, entry) {
     if(item.completed) {
         completedItemsCount--;
         UpdateTaskCounter();
     }
     
-    let index;
-    for(i = 0; i < listItems.length; i++) {
-        if(listItems[i] == item) {
-            index = i;
-            break;
-        }
-    }
+    let text = entry.firstChild.textContent;
+    let index = listItems.map(t => t.name).indexOf(text);
+
+    entry.remove();
     listItems.splice(index, 1);
 }
 
